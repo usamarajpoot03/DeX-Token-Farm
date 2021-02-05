3a2083046a78f399297660062068bb3057b87350
usama <usamarajpoot03@yahoo.com>
2021-02-06 01:45:36 +0500

    Updates

---
+++ precache-manifest.e2e66068cbab44952931f3f7627b46ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72632993a1a8293cc2a9749971d8e8db",
+    "url": "/Token-Farming/index.html"
+  },
+  {
+    "revision": "be881523ef63fa42c3f3",
+    "url": "/Token-Farming/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5728b67f33c2d235e90c",
+    "url": "/Token-Farming/static/js/2.912d9b7f.chunk.js"
+  },
+  {
+    "revision": "be881523ef63fa42c3f3",
+    "url": "/Token-Farming/static/js/main.64a5493f.chunk.js"
+  },
+  {
+    "revision": "ca10a6cffa89e2a3cfdd",
+    "url": "/Token-Farming/static/js/runtime-main.01f2331b.js"
+  }
+]);
